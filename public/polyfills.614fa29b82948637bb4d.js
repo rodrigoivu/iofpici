d2a71cd51c02409bad2cf008534df1863cc1f4c6
Rodrigo <rvaras@ceapro.cl>
2020-10-24 10:43:12 -0300

    prueba reload

--- public/polyfills.614fa29b82948637bb4d.js
+++ public/polyfills.614fa29b82948637bb4d.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\WWW\IOFISH\FRONT-IOFISH-V2\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\WWW\IOFISH\starterkit\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
